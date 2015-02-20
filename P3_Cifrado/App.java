4f72d1737f3c630b68fb3cb65d8bfab44a7acb49
EverCedillo <EverCedillo@users.noreply.github.com>
2015-02-19 20:34:08 -0600

    Agregado alfabeto completo

--- P3_Cifrado/App.java
+++ P3_Cifrado/App.java
@@ -1,27 +1,32 @@
 import java.util.*;
 class App {
 	public static void main(String[] args) {
-		Encryption en = new Encryption();
+		Encryption en = new Encryption(new String());
 		System.out.println(en.toString());
 	}
 	
 }
 
 class Encryption{
-	public static final String alphabet[]={"a","b","c","d","e","f","g","h"};
-	String[] key = new String[alphabet.length];
-	Encryption(){
+	public static final String ALPHABET[]={"a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","w","y","z"};
+	private String[] key = new String[ALPHABET.length];
+	String info;
+	Encryption(String info){
 		buildKey();
+		this.info=info;
 	}	
 	void buildKey(){
-		List<String> temp=Arrays.asList(alphabet.clone());
+		List<String> temp=Arrays.asList(ALPHABET.clone());
 		Collections.shuffle(temp);
 		temp.toArray(key);
 	}
+	void encrypt(){
+
+	}
 	public String toString(){
 		String str="";
-		for(int i=0;i<8;i++)
-			str=str+" "+alphabet[i]+"-"+key[i];
+		for(int i=0;i<ALPHABET.length;i++)
+			str=str+" "+ALPHABET[i]+"-"+key[i];
 		return str;
 	}
 }
