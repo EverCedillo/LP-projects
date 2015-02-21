b8dd825d3cd49bea1d39db962cc6bf7e31c5e6ae
EverCedillo <EverCedillo@users.noreply.github.com>
2015-02-21 02:30:41 -0600

    Encriptado terminado, falta toString y comentarios

--- P3_Cifrado/App.java
+++ P3_Cifrado/App.java
@@ -1,8 +1,8 @@
 import java.util.*;
 class App {
 	public static void main(String[] args) {
-		Encryption en = new Encryption(new String());
-		System.out.println(en.toString());
+		Encryption en = new Encryption("holo Como estos");
+		System.out.println(en.toString()+"\n-"+en.infoEncrypt+"-");
 	}
 	
 }
@@ -11,9 +11,11 @@ class Encryption{
 	public static final String ALPHABET[]={"a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","w","y","z"};
 	private String[] key = new String[ALPHABET.length];
 	String info;
+	String infoEncrypt="";
 	Encryption(String info){
 		buildKey();
-		this.info=info;
+		this.info=info.toLowerCase();
+		encrypt();
 	}	
 	void buildKey(){
 		List<String> temp=Arrays.asList(ALPHABET.clone());
@@ -21,7 +23,9 @@ class Encryption{
 		temp.toArray(key);
 	}
 	void encrypt(){
-
+		String infoNoSpaces=info.replace(" ","");
+		for(int i=0; i<infoNoSpaces.length();i++)
+			infoEncrypt+=key[Arrays.asList(ALPHABET).indexOf(String.valueOf(infoNoSpaces.charAt(i)))];
 	}
 	public String toString(){
 		String str="";
