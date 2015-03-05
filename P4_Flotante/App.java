caec323ed7893cf5b4c063e51c1cb7c3d60c27d0
EverCedillo <EverCedillo@users.noreply.github.com>
2015-03-05 17:09:15 -0600

    Terminado algoritmo

--- P4_Flotante/App.java
+++ P4_Flotante/App.java
@@ -26,18 +26,34 @@ class FloatBinary {
 		buildBinary();
 	}
 	void buildBinary(){
+		List<Float> tmp = new ArrayList<Float>();
 		long integ = (long)Math.abs(num);
 		long index = (long)Math.floor(Math.log(Math.abs(integ))/Math.log(2));
 		float frac = num - integ;
-		for(int i=0;i<index+1;i++)
-			binary.add(((integ>>i)&1)==1);
-		for(int i=0;i<8;i++)
-			binary.add((((index+127)>>i)&1)==1);
+		float el = frac;
+		//binary.add(Math.abs(el)>=1);
+
 		binary.add(num<0);
+		for(int i=7;i>=0;i--)
+			binary.add((((index+127)>>i)&1)==1);
+		for(long i=index;i>=0;i--)
+			binary.add(((integ>>i)&1)==1);
+
+		while(tmp.size()<23&&tmp.indexOf(el)==-1){
+			//System.out.println(el+"--\n");
+			
+			tmp.add(el);
+			System.out.println(el+"--tmp\n");
+			el=el*2;
+			binary.add(Math.abs(el)>=1);
+			System.out.println(el+"--bool\n");
+			el=el- (long)el;
+			//System.out.println(el+"\n");
+		}
 	}
 	public String toString(){
 		String str="";
-		for(int i=binary.size()-1;i>=0;i--)
+		for(int i=0;i<binary.size();i++)
 			str=str+""+(binary.get(i)?1:0);
 		return str;
 	}
