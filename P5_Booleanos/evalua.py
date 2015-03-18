5a60cbeb57fa8194450a3bc9e5bfe422a2ec5571
Brenda Garcia <fi.brendag@hotmail.com>
2015-03-17 23:41:30 -0600

    Versión final P5

--- P5_Booleanos/evalua.py
+++ P5_Booleanos/evalua.py
@@ -1,21 +1,33 @@
+import math
 def evalua(expresion):
   data = expresion.split()
-  #Las expresiones deben estar separadas por espacios en blanco
-  dicOp = {'+':'+', '-':'-','*':'*','/':'/','^':'**','res':'%','y':'and','v':'or','n':'not','<=':'<=','>=':'>=','==':'==','!=':'!='}
-  dicOpU = {}
+  dicOp = {'+':'+', '-':'-','*':'*','/':'/','^':'**','res':'%','and':'and','or':'or','<=':'<=','>=':'>=','==':'==','!=':'!=','<':'<','>':'>'}
+  dicOpU = {'sqrt':'sqrt','sin':'sin','cos':'cos','atan':'atan','tan':'tan','acos':'acos'}
+  dicOpN ={'not':'not'}
   ops = []
   val = []
   for i in data:
     if i == '(':
-      pass #ignorar
+      pass
     elif i in dicOp:
       ops.append(dicOp[i])
+    elif i in dicOpU:
+      ops.append(dicOpU[i])
+    elif i in dicOpN:
+      ops.append(dicOpN[i])
     elif i == ')':
       op = ops.pop()
-      n2 = val.pop()
-      n1 = val.pop()
-      #print str(n1) + op + str(n2)
-      va = eval(str(n1)+ " "+op+ " "+str(n2))
+      if op in dicOp:
+        n2 = val.pop()
+        n1 = val.pop()
+	print str(n1)+ op + str(n2)
+        va = eval(str(n1)+ " "+ op + " "+str(n2))
+      if op in dicOpN:
+          n1 = val.pop()
+          va = eval(op+"("+str(n1)+")")
+      if op in dicOpU:
+          n1 = val.pop()
+          va = eval("math."+op+"("+str(n1)+")")
       val.append(va)
     else:
       val.append(float(i))
