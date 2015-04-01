25b34264574da530bf537881cbe80d362ec0fcde
Brenda Garcia <fi.brendag@hotmail.com>
2015-03-31 22:43:27 -0600

    P6: Corrección del cálculo del ángulo

--- P6_Direc/P6.py
+++ P6_Direc/P6.py
@@ -2,13 +2,19 @@ import math
 def distances(l):
 	d=[]
 	for i in range(len(l)-1):
-		mag= math.sqrt((l[i][0] - l[i+1][0])**2 + (l[i][1] - l[i+1][1])**2)
-		angle=math.acos((l[i][0]*l[i+1][0]+l[i][1]*l[i+1][1])/(math.sqrt((l[i][0]**2 + l[i+1][0]**2)*(l[i][1]**2 + l[i+1][1]**2))))
-		d.append((mag,math.degrees(angle)))
+	    mag= math.sqrt((l[i][0] - l[i+1][0])**2 + (l[i][1] - l[i+1][1])**2)
+            x=l[i+1][0] - l[i][0]
+            if x == 0:
+               d.append((mag,90))
+            else:
+	       angle=math.atan2((l[i+1][1] - l[i][1]),x)
+	       d.append((mag,math.degrees(angle)))
 	return d
+
 def reverse(l):
 	d=[]
 	for i in l.__reversed__():
 		d.append(i)
 	return d
 
+	
