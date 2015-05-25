91d07bdf8c7eec1c68f445fcba4d3da7bda9f991
EverCedillo <EverCedillo@users.noreply.github.com>
2015-05-24 23:26:13 -0500

    P7_agregado eval y subst

--- P7_expAritm/exp.rkt
+++ P7_expAritm/exp.rkt
@@ -19,7 +19,20 @@
                   (eval (sum-der en))))
     ((mul? en) (*(eval (mul-izq en))
                   (eval (mul-der en))))
-    (else "Error")
+    (else "No se puede evaluar")
     ))
+
+(define (subst e s v)
+  (cond
+     ((eq? s e) v)
+     ((number? e) e)
+     ((sum? e) (make-sum (subst (sum-izq e) s v)
+                         (subst (sum-der e) s v)))
+     ((mul? e) (make-mul (subst (mul-izq e) s v)
+                         (subst (mul-der e) s v)))))             
+    
+  
           
 (define e (make-sum (make-mul 2 3) 4))
+(define es (make-sum (make-mul 'x 3) 'x))
+  
